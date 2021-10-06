a48a7fe5ae6481e6d5a4cc0867fa86804e571a89
MatthieuJBM <m.baryla@protonmail.ch>
2021-10-06 16:54:46 +0200

    V3

--- Hallo/src/usothreads/Hallo.java
+++ Hallo/src/usothreads/Hallo.java
@@ -7,6 +7,8 @@ public class Hallo {
         int s = 15;
         String a = "a";
         System.out.println(s + " " + a);
-        
+        int r = 3;
+        int rs = s*r;
+        System.out.println(rs);
     }
 }
