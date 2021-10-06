0af27410131a6235ee5e7e86295041f351951878
MatthieuJBM <m.baryla@protonmail.ch>
2021-10-06 16:48:09 +0200

    V1

---
+++ Hallo/src/usothreads/Hallo.java
@@ -0,0 +1,12 @@
+package usothreads;
+
+public class Hallo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola!");
+        int s = 15;
+        String a = "a";
+        System.out.println(s + " " + a);
+        String b = "b";
+    }
+}
